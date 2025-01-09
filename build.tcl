8270f57e97224ccd42d165fa7e4228871fa62b9d
Danilo Chang <ray2501@gmail.com>
2025-01-09 11:12:38 +0800

    Update

--- build.tcl
+++ build.tcl
@@ -4,7 +4,7 @@ set arch "x86_64"
 set base "VecTcl-0.3"
 set fileurl "https://github.com/auriocus/VecTcl/archive/v0.3.tar.gz"
 
-set var [list wget $fileurl -O $base.tar.gz]
+set var [list wget2 $fileurl -O $base.tar.gz]
 exec >@stdout 2>@stderr {*}$var
 
 if {[file exists build]} {
